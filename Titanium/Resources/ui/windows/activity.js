3108391f323b8bf17a66fb7623bbbb94650059ca
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-15 19:06:45 -0700

    add checks for iphone specific code, remove map, remove old test file

--- Titanium/Resources/ui/windows/activity.js
+++ Titanium/Resources/ui/windows/activity.js
@@ -10,23 +10,25 @@ var webview = Titanium.UI.createWebView({
   url: url
 });
 
-var refresh = Ti.UI.createButton({
-  systemButton: Ti.UI.iPhone.SystemButton.REFRESH
-});
+if(Ti.Platform.osname === "iphone"){
+	var refresh = Ti.UI.createButton({
+		systemButton: Ti.UI.iPhone.SystemButton.REFRESH
+	});
 
-var loading = Ti.UI.createButton({
-  systemButton: Ti.UI.iPhone.SystemButton.ACTIVITY
-});
+	var loading = Ti.UI.createButton({
+		systemButton: Ti.UI.iPhone.SystemButton.ACTIVITY
+	});
 
-webview.addEventListener("load", function(){
-	activityWindow.setLeftNavButton(null);
-});
+	webview.addEventListener("load", function(){
+		activityWindow.setLeftNavButton(null);
+	});
 
-refresh.addEventListener("click", function(e){
-  activityWindow.setLeftNavButton(loading);
-  webview.reload();
-});
+	refresh.addEventListener("click", function(e){
+		activityWindow.setLeftNavButton(loading);
+		webview.reload();
+	});
 
-activityWindow.setLeftNavButton(loading);
-activityWindow.setRightNavButton(refresh);
+	activityWindow.setLeftNavButton(loading);
+	activityWindow.setRightNavButton(refresh);
+}
 Titanium.UI.currentWindow.add(webview);
